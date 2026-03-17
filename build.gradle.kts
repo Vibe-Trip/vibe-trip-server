74161f99d16d4c44f1bc5e90c8a6a5d130fc3015
rt3310 <btac3310@gmail.com>
2026-03-17 19:25:01 +0900

    feature[#1]: security, jwt 관련 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,10 +42,19 @@ dependencies {
     runtimeOnly("io.micrometer:micrometer-registry-prometheus")
 
     // Log
-    api("io.github.microutils:kotlin-logging-jvm:3.0.5")
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
 
     // Api Docs
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.14")
+
+    // Security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")

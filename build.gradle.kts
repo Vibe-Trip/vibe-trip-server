4c14b34820a955ad779c3200a1bd43ce92606cdc
rt3310 <btac3310@gmail.com>
2026-03-18 22:46:00 +0900

    test[#1]: OAuth Login 테스트 코드 작성

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,7 +74,9 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:${kotestVersion}")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:${kotestVersion}")
+    testImplementation("io.mockk:mockk:${mockKVersion}")}
 
 kotlin {
     compilerOptions {

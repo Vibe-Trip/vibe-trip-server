e3c5c94db4128401f732e64b951c6d92a1804d18
mkkim21 <dgd5980@naver.com>
2026-03-19 23:34:53 +0900

    feature[#3]: 앨범 및 앨범 뮤직 레포지토리 생성

---
+++ src/main/kotlin/com/vibetrip/vibetripserver/album/dataaccess/repository/AlbumMusicRepository.kt
@@ -0,0 +1,7 @@
+package com.vibetrip.vibetripserver.album.dataaccess.repository
+
+import com.vibetrip.vibetripserver.album.dataaccess.entity.AlbumMusicEntity
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface AlbumMusicRepository : JpaRepository<AlbumMusicEntity, Long>{
+}

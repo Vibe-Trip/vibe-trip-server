d409e872843e4b1334deeebddee45b9c961f40fc
mkkim21 <dgd5980@naver.com>
2026-03-20 16:57:35 +0900

    refactor[#3]: 앨범 vo 상수 선언 방식으로 개선

--- src/main/kotlin/com/vibetrip/vibetripserver/album/domain/vo/Comment.kt
+++ src/main/kotlin/com/vibetrip/vibetripserver/album/domain/vo/Comment.kt
@@ -5,8 +5,11 @@ import com.vibetrip.vibetripserver.common.exception.ErrorType
 
 @JvmInline
 value class Comment(val value: String) {
+    companion object{
+        private const val MAX_COMMENT_LENGTH = 500
+    }
     init {
-        if(value.length > 500) {
+        if(value.length > MAX_COMMENT_LENGTH) {
             throw AppException(ErrorType.INVALID_ALBUM_COMMENT)
         }
     }
--- src/main/kotlin/com/vibetrip/vibetripserver/album/domain/vo/Region.kt
+++ src/main/kotlin/com/vibetrip/vibetripserver/album/domain/vo/Region.kt
@@ -5,8 +5,11 @@ import com.vibetrip.vibetripserver.common.exception.ErrorType
 
 @JvmInline
 value class Region(val value: String) {
+    companion object {
+        private const val MAX_REGION_LENGTH = 15
+    }
     init {
-        if(value.isBlank() || value.length > 15){
+        if(value.isBlank() || value.length > MAX_REGION_LENGTH) {
             throw AppException(ErrorType.INVALID_ALBUM_REGION)
         }
     }

aeb2c7eb6c8d44a3011d46030a38fdb69536da1f
mkkim21 <dgd5980@naver.com>
2026-03-20 16:50:43 +0900

    feature[#3]: 앨범 생성 비즈니스 로직 구현

--- src/main/kotlin/com/vibetrip/vibetripserver/album/business/AlbumMusicService.kt
+++ src/main/kotlin/com/vibetrip/vibetripserver/album/business/AlbumMusicService.kt
@@ -1,4 +1,26 @@
 package com.vibetrip.vibetripserver.album.business
 
-class AlbumMusicService {
+import com.vibetrip.vibetripserver.album.domain.NewAlbumMusic
+import com.vibetrip.vibetripserver.album.implement.AlbumManager
+import com.vibetrip.vibetripserver.album.implement.AlbumMusicManager
+import com.vibetrip.vibetripserver.album.implement.music.MusicGenerator
+import com.vibetrip.vibetripserver.album.implement.music.TitleGenerator
+import org.springframework.scheduling.annotation.Async
+import org.springframework.stereotype.Service
+
+@Service
+class AlbumMusicService(
+    private val albumManager: AlbumManager,
+    private val albumMusicManager: AlbumMusicManager,
+    private val musicGenerator: MusicGenerator,
+    private val titleGenerator: TitleGenerator
+) {
+    @Async
+    fun generate(newAlbumMusic: NewAlbumMusic) {
+        val title = titleGenerator.generate(newAlbumMusic)
+        albumManager.updateTitle(newAlbumMusic.albumId, title)
+
+        val music = musicGenerator.generate(newAlbumMusic)
+        albumMusicManager.save(newAlbumMusic, music)
+    }
 }
--- src/main/kotlin/com/vibetrip/vibetripserver/album/business/AlbumService.kt
+++ src/main/kotlin/com/vibetrip/vibetripserver/album/business/AlbumService.kt
@@ -1,4 +1,20 @@
 package com.vibetrip.vibetripserver.album.business
 
-class AlbumService {
+import com.vibetrip.vibetripserver.album.domain.NewAlbum
+import com.vibetrip.vibetripserver.album.domain.NewAlbumMusic
+import com.vibetrip.vibetripserver.album.implement.AlbumManager
+import com.vibetrip.vibetripserver.album.presentation.dto.AlbumCreateResponse
+import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+
+@Service
+class AlbumService(
+    private val albumManager: AlbumManager,
+    private val albumMusicService: AlbumMusicService,
+) {
+    fun  create(newAlbum: NewAlbum, image: MultipartFile?): AlbumCreateResponse {
+        val albumId = albumManager.create(newAlbum)
+        albumMusicService.generate(NewAlbumMusic.of(albumId, newAlbum))
+        return AlbumCreateResponse(albumId)
+    }
 }

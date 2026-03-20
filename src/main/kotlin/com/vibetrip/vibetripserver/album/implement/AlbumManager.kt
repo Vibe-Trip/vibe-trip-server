0a3b4af0ed69bfe619fccef9976a951510229f5a
mkkim21 <dgd5980@naver.com>
2026-03-20 16:48:16 +0900

    feature[#3]: 앨범 생성 implement 구현

--- src/main/kotlin/com/vibetrip/vibetripserver/album/implement/AlbumManager.kt
+++ src/main/kotlin/com/vibetrip/vibetripserver/album/implement/AlbumManager.kt
@@ -1,4 +1,24 @@
 package com.vibetrip.vibetripserver.album.implement
 
-class AlbumManager {
+import com.vibetrip.vibetripserver.album.dataaccess.entity.AlbumEntity
+import com.vibetrip.vibetripserver.album.dataaccess.repository.AlbumRepository
+import com.vibetrip.vibetripserver.album.domain.NewAlbum
+import com.vibetrip.vibetripserver.common.exception.AppException
+import com.vibetrip.vibetripserver.common.exception.ErrorType
+import jakarta.transaction.Transactional
+import org.springframework.stereotype.Component
+
+@Component
+@Transactional
+class AlbumManager(
+    private val albumRepository: AlbumRepository,
+) {
+    fun create(newAlbum: NewAlbum): Long =
+        albumRepository.save(AlbumEntity.from(newAlbum)).id!!
+
+    fun updateTitle(albumId: Long, title: String) {
+        albumRepository.findById(albumId)
+            .orElseThrow { AppException(ErrorType.NOT_FOUND_ALBUM)}
+            .updateTitle(title)
+    }
 }
--- src/main/kotlin/com/vibetrip/vibetripserver/album/implement/AlbumMusicManager.kt
+++ src/main/kotlin/com/vibetrip/vibetripserver/album/implement/AlbumMusicManager.kt
@@ -1,4 +1,18 @@
 package com.vibetrip.vibetripserver.album.implement
 
-class AlbumMusicManager {
+import com.vibetrip.vibetripserver.album.dataaccess.entity.AlbumMusicEntity
+import com.vibetrip.vibetripserver.album.dataaccess.repository.AlbumMusicRepository
+import com.vibetrip.vibetripserver.album.domain.GenerateMusic
+import com.vibetrip.vibetripserver.album.domain.NewAlbumMusic
+import jakarta.transaction.Transactional
+import org.springframework.stereotype.Component
+
+@Component
+@Transactional
+class AlbumMusicManager(
+    private val albumMusicRepository: AlbumMusicRepository,
+) {
+    fun save(newAlbumMusic: NewAlbumMusic, music: GenerateMusic) {
+        albumMusicRepository.save(AlbumMusicEntity.from(newAlbumMusic, music))
+    }
 }

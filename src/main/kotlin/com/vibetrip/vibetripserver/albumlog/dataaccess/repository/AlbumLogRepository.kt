190f1af398b2edaa59c257fb36b2f013f4a13629
bbie-6772 <bbie6772@daum.net>
2026-03-23 16:38:03 +0900

    style[#10]: ktlinFormat 적용

--- src/main/kotlin/com/vibetrip/vibetripserver/albumlog/dataaccess/repository/AlbumLogRepository.kt
+++ src/main/kotlin/com/vibetrip/vibetripserver/albumlog/dataaccess/repository/AlbumLogRepository.kt
@@ -3,5 +3,6 @@ package com.vibetrip.vibetripserver.albumlog.dataaccess.repository
 import com.vibetrip.vibetripserver.albumlog.dataaccess.entity.AlbumLogEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface AlbumLogRepository : JpaRepository<AlbumLogEntity, Long>,
+interface AlbumLogRepository :
+    JpaRepository<AlbumLogEntity, Long>,
     CustomAlbumLogRepository
--- src/main/kotlin/com/vibetrip/vibetripserver/albumlog/dataaccess/repository/CustomAlbumLogRepositoryImpl.kt
+++ src/main/kotlin/com/vibetrip/vibetripserver/albumlog/dataaccess/repository/CustomAlbumLogRepositoryImpl.kt
@@ -1,26 +1,24 @@
 package com.vibetrip.vibetripserver.albumlog.dataaccess.repository
 
+import org.springframework.stereotype.Repository
 import com.vibetrip.vibetripserver.albumlog.dataaccess.entity.AlbumLogEntity
 import com.vibetrip.vibetripserver.albumlog.dataaccess.entity.QAlbumLogEntity.albumLogEntity
 import com.vibetrip.vibetripserver.album.dataaccess.entity.QAlbumEntity.albumEntity
 import com.vibetrip.vibetripserver.common.enums.EntityStatus
 import com.vibetrip.vibetripserver.support.querydsl.QuerydslRepositorySupport
-import org.springframework.stereotype.Repository
-
 
 @Repository
 class CustomAlbumLogRepositoryImpl :
     QuerydslRepositorySupport(AlbumLogEntity::class),
     CustomAlbumLogRepository {
-
     override fun countByMemberKey(memberKey: String): Long =
         select(albumLogEntity.count())
             .from(albumLogEntity)
-            .join(albumEntity).on(albumLogEntity.albumId.eq(albumEntity.id))
+            .join(albumEntity)
+            .on(albumLogEntity.albumId.eq(albumEntity.id))
             .where(
                 albumEntity.memberKey.eq(memberKey),
                 albumEntity.status.eq(EntityStatus.ACTIVE),
                 albumLogEntity.status.eq(EntityStatus.ACTIVE),
-            )
-            .fetchOne() ?: 0L
+            ).fetchOne() ?: 0L
 }

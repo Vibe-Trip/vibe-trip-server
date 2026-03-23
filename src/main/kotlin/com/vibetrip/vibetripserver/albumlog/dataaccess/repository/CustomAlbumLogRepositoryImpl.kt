225c90ecf485485896902fedbea4a7ce3652ee7e
bbie-6772 <bbie6772@daum.net>
2026-03-23 16:46:35 +0900

    style[#10]: ktlinFormat 적용

--- src/main/kotlin/com/vibetrip/vibetripserver/albumlog/dataaccess/repository/CustomAlbumLogRepositoryImpl.kt
+++ src/main/kotlin/com/vibetrip/vibetripserver/albumlog/dataaccess/repository/CustomAlbumLogRepositoryImpl.kt
@@ -1,11 +1,12 @@
 package com.vibetrip.vibetripserver.albumlog.dataaccess.repository
 
-import org.springframework.stereotype.Repository
+
+import com.vibetrip.vibetripserver.album.dataaccess.entity.QAlbumEntity.albumEntity
 import com.vibetrip.vibetripserver.albumlog.dataaccess.entity.AlbumLogEntity
 import com.vibetrip.vibetripserver.albumlog.dataaccess.entity.QAlbumLogEntity.albumLogEntity
-import com.vibetrip.vibetripserver.album.dataaccess.entity.QAlbumEntity.albumEntity
 import com.vibetrip.vibetripserver.common.enums.EntityStatus
 import com.vibetrip.vibetripserver.support.querydsl.QuerydslRepositorySupport
+import org.springframework.stereotype.Repository
 
 @Repository
 class CustomAlbumLogRepositoryImpl :

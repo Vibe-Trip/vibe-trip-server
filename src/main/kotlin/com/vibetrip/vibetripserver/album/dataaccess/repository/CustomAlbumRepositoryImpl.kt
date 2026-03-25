d71f26935f8bb60187a1ba6f74ee7ae1da165ee6
mkkim21 <dgd5980@naver.com>
2026-03-26 03:52:55 +0900

    feature[#2]: 앪범 목록 조회 레포지토리 구현

---
+++ src/main/kotlin/com/vibetrip/vibetripserver/album/dataaccess/repository/CustomAlbumRepositoryImpl.kt
@@ -0,0 +1,46 @@
+package com.vibetrip.vibetripserver.album.dataaccess.repository
+
+import com.vibetrip.vibetripserver.album.dataaccess.entity.AlbumEntity
+import com.vibetrip.vibetripserver.album.dataaccess.entity.QAlbumEntity.albumEntity
+import com.vibetrip.vibetripserver.common.enums.EntityStatus
+import com.vibetrip.vibetripserver.support.paging.Cursorable
+import com.vibetrip.vibetripserver.support.paging.Slice
+import com.vibetrip.vibetripserver.support.querydsl.QuerydslRepositorySupport
+
+class CustomAlbumRepositoryImpl :
+    QuerydslRepositorySupport(AlbumEntity::class),
+    CustomAlbumRepository {
+    override fun find(albumId: Long) =
+        selectFrom(albumEntity)
+            .where(
+                albumEntity.id.eq(albumId),
+                albumEntity.status.eq(EntityStatus.ACTIVE),
+            ).fetchOne()
+
+    override fun findAllByMemberKey(
+        memberKey: String,
+        cursorable: Cursorable<Long>,
+    ): Slice<AlbumEntity> {
+        val content =
+            selectFrom(albumEntity)
+                .where(
+                    albumEntity.memberKey.eq(memberKey),
+                    albumEntity.status.eq(EntityStatus.ACTIVE),
+                    ltCursor(cursorable.limit + 1L),
+                ).orderBy(albumEntity.id.desc())
+                .limit(cursorable.limit + 1L)
+                .fetch()
+
+        return Slice(content, cursorable, hasNext(cursorable, content))
+    }
+
+    override fun countByMemberKey(memberKey: String) =
+        select(albumEntity.count())
+            .from(albumEntity)
+            .where(
+                albumEntity.memberKey.eq(memberKey),
+                albumEntity.status.eq(EntityStatus.ACTIVE),
+            ).fetchOne() ?: 0L
+
+    private fun ltCursor(cursor: Long?) = cursor?.let { albumEntity.id.lt(it) }
+}

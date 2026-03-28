c9ae350862a420a3ddf2d152bdf894157160102c
rt3310 <btac3310@gmail.com>
2026-03-28 22:24:20 +0900

    test[#36]: 통합 테스트 환경 분리

--- src/test/kotlin/com/vibetrip/vibetripserver/support/integration/SpringTest.kt
+++ src/test/kotlin/com/vibetrip/vibetripserver/support/integration/SpringTest.kt
@@ -1,12 +1,11 @@
 package com.vibetrip.vibetripserver.support.integration
 
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.mysql.MySQLContainer
 
-@SpringBootTest
+@IntegrationTest
 @ActiveProfiles("test")
 class SpringTest {
     companion object {

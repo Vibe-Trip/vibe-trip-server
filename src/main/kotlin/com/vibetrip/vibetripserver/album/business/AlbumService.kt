4a047806fa36be43f1d136af6e8e23ce63669551
mkkim21 <dgd5980@naver.com>
2026-04-01 02:50:24 +0900

    feature[#5]: 앨범 삭제 API 구현

--- src/main/kotlin/com/vibetrip/vibetripserver/album/business/AlbumService.kt
+++ src/main/kotlin/com/vibetrip/vibetripserver/album/business/AlbumService.kt
@@ -4,6 +4,7 @@ import com.vibetrip.vibetripserver.album.domain.NewAlbum
 import com.vibetrip.vibetripserver.album.implement.AiProcessor
 import com.vibetrip.vibetripserver.album.implement.AlbumCoverImageProcessor
 import com.vibetrip.vibetripserver.album.implement.AlbumManager
+import com.vibetrip.vibetripserver.album.implement.AlbumMemberManager
 import com.vibetrip.vibetripserver.album.presentation.dto.response.AlbumCreateResponse
 import com.vibetrip.vibetripserver.support.paging.Cursorable
 import org.springframework.stereotype.Service
@@ -12,6 +13,7 @@ import org.springframework.web.multipart.MultipartFile
 
 @Service
 class AlbumService(
+    private val albumMemberManager: AlbumMemberManager,
     private val albumManager: AlbumManager,
     private val aiProcessor: AiProcessor,
     private val albumCoverImageProcessor: AlbumCoverImageProcessor,
@@ -38,4 +40,13 @@ class AlbumService(
     ) = albumManager.find(memberKey, cursorable)
 
     fun countAlbums(memberKey: String): Long = albumManager.count(memberKey)
+
+    @Transactional
+    fun deleteAlbum(
+        albumId: Long,
+        memberKey: String,
+    ) {
+        albumMemberManager.validateMember(albumId, memberKey)
+        albumManager.delete(albumId)
+    }
 }

d78d48eb6b4dbe8e12543b2cab4d2ed4d63374c7
rt3310 <btac3310@gmail.com>
2026-04-06 22:45:12 +0900

    refactor[#9]: Base64 package 사용 변경(java -> kotlin)

--- src/main/kotlin/com/vibetrip/vibetripserver/config/cloud/FCMConfig.kt
+++ src/main/kotlin/com/vibetrip/vibetripserver/config/cloud/FCMConfig.kt
@@ -6,11 +6,11 @@ import com.google.firebase.FirebaseOptions
 import jakarta.annotation.PostConstruct
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
-import java.util.Base64
+import kotlin.io.encoding.Base64
 
 @Configuration
 class FCMConfig(
-    @Value("\${firebase.key}")
+    @Value($$"${firebase.key}")
     private val credentialsBase64: String,
 ) {
     @PostConstruct
@@ -19,7 +19,7 @@ class FCMConfig(
 
         val credentials =
             GoogleCredentials.fromStream(
-                Base64.getDecoder().decode(credentialsBase64).inputStream(),
+                Base64.decode(credentialsBase64).inputStream(),
             )
         FirebaseApp.initializeApp(
             FirebaseOptions

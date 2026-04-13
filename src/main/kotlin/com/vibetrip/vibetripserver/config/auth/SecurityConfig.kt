0c3376b467cab4aff2f13cf86edc5b29b76cde61
rt3310 <btac3310@gmail.com>
2026-04-14 02:24:52 +0900

    style[#101]: ktlint format 적용

--- src/main/kotlin/com/vibetrip/vibetripserver/config/auth/SecurityConfig.kt
+++ src/main/kotlin/com/vibetrip/vibetripserver/config/auth/SecurityConfig.kt
@@ -12,7 +12,6 @@ import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher.withDefaults
 
 @Configuration
 class SecurityConfig(
